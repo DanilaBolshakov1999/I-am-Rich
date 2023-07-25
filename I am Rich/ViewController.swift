1e5b2f1fa97176228d1fa60d14ff572228b55aae
Danila Bolshakov <danila1999777@gmail.com>
2023-07-25 13:01:51 +0300

    Add image files

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Danila Bolshakov on 09.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
